f06dcf95c8ff4c0628c3e6132afbbbe394e549dd
Lwin Ye <lwin_ye@yahoo.com>
2020-06-14 23:59:45 -0700

    created new blog module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       'mongodb+srv://admin:Pokidex@pokedex-kaf7p.mongodb.net/pokedex?retryWrites=true&w=majority',
       { useNewUrlParser: true },
     ),
+    BlogModule,
   ],
   controllers: [AppController],
   providers: [AppService],
